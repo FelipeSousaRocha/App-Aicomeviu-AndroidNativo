1cd8afe8cec10a3e4d313da25351221485d269d5
FelipeSousaRocha <felipedsousarocha@gmail.com>
2022-07-23 18:11:56 -0300

    1

---
+++ app/src/main/java/com/android/foodorderapp/OrderSucceessActivity.java
@@ -0,0 +1,38 @@
+package com.android.foodorderapp;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.foodorderapp.model.RestaurantModel;
+
+public class OrderSucceessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_succeess);
+
+
+        RestaurantModel restaurantModel = getIntent().getParcelableExtra("RestaurantModel");
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ffa500")));
+        actionBar.setTitle(restaurantModel.getName());
+        actionBar.setSubtitle(restaurantModel.getAddress());
+        actionBar.setDisplayHomeAsUpEnabled(false);
+
+
+        TextView buttonDone = findViewById(R.id.buttonDone);
+        buttonDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
